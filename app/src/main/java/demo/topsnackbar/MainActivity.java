2b491a863f1ad89a4d690030d423d909647308df
woniu0936 <275949034@qq.com>
2017-05-08 15:12:20 +0800

    modify

--- app/src/main/java/demo/topsnackbar/MainActivity.java
+++ app/src/main/java/demo/topsnackbar/MainActivity.java
@@ -1,6 +1,7 @@
 package demo.topsnackbar;
 
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -11,15 +12,18 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private ConstraintLayout content;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        content = (ConstraintLayout) findViewById(R.id.cl_content);
         findViewById(R.id.tv_hello).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "hello world is onClick!");
-                TopSnackbar.make(getWindow().getDecorView(), "说的发送大公司是多个电饭锅电饭锅地方郭德纲水电费费郭德纲电饭锅地方高档房个的风格的", TopSnackbar.LENGTH_SHORT).show();
+                TopSnackbar.make(content, "说的发送大公司是多个电饭锅电饭锅地方郭德纲水电费费郭德纲电饭锅地方高档房个的风格的", TopSnackbar.LENGTH_SHORT).show();
             }
         });
     }
